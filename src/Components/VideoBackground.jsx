7b5583d7e4c16f8ebd12f02e5f0fabed9b5644e0
Mitch Fox <mjfox99@gmail.com>
2022-05-19 23:47:00 +1000

    fixed mp4

--- src/Components/VideoBackground.jsx
+++ src/Components/VideoBackground.jsx
@@ -1,31 +1,52 @@
-import React, {useRef, useEffect} from "react"
-import '../App.css';
+// import React, {useRef, useEffect} from "react"
+// import '../App.css';
 import Video from '../Assets/jedacai.mp4';
 
-function VideoBackground() {
+// function VideoBackground() {
 
+//   const videoRef = useRef(undefined);
+//   useEffect(() => {
+//       videoRef.current.defaultMuted = true;
+//   })
+
+//   return (
+//     <div className='videoContainer'>
+//       <video
+//         loop
+//         autoPlay
+//         muted
+//         controls={false}
+//         ref={videoRef}
+//         preLoad="auto"
+//         className='jedVideo'>
+//         <source
+//           src={Video}
+//           type="video/mp4"
+//         />
+//       </video>
+//     </div>
+//   );
+// }
+
+// export default VideoBackground;
+
+import React, { useRef, useEffect } from "react"
+
+export default function AutoPlaySilentVideo(props) {
   const videoRef = useRef(undefined);
   useEffect(() => {
-      videoRef.current.defaultMuted = true;
+    videoRef.current.defaultMuted = true;
   })
-
   return (
     <div className='videoContainer'>
       <video
+        ref={videoRef}
         loop
         autoPlay
         muted
-        controls
-        ref={videoRef}
-        preLoad="auto"
-        className='jedVideo'>
-        <source
-          src={Video}
-          type="video/mp4"
-        />
+        playsInline>
+        <source src={Video} type="video/mp4" />
       </video>
     </div>
   );
 }
-
-export default VideoBackground;
