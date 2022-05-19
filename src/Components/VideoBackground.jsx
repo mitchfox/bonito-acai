73fc4f0c468c91fd71840bdd699174dce74eaa22
Mitch Fox <mjfox99@gmail.com>
2022-05-19 23:23:15 +1000

    mp4 fix

--- src/Components/VideoBackground.jsx
+++ src/Components/VideoBackground.jsx
@@ -1,16 +1,30 @@
+import React, {useRef, useEffect} from "react"
 import '../App.css';
 import Video from '../Assets/jedacai.mp4';
 
 function VideoBackground() {
+
+  const videoRef = useRef(undefined);
+  useEffect(() => {
+      videoRef.current.defaultMuted = true;
+  })
+
   return (
     <div className='videoContainer'>
-    <video loop autoPlay muted className='jedVideo'>
-      <source
-        src={Video}
-        type="video/mp4"
-      />
-    </video>
-  </div>
+      <video
+        loop
+        autoPlay
+        muted
+        controls
+        ref={videoRef}
+        preLoad="auto"
+        className='jedVideo'>
+        <source
+          src={Video}
+          type="video/mp4"
+        />
+      </video>
+    </div>
   );
 }
 
